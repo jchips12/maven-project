58d4c2d0c8b78152319a45c043cb1620c81b488f
JC Hipolito <jchips12@gmail.com>
2018-05-28 17:30:16 +0800

    remove tabs

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -8,20 +8,20 @@ package com.example;
  */
 public class Greeter {
 
-	/**
-	 * This is a constructor.
-	 */
-	public Greeter() {
+  /**
+   * This is a constructor.
+   */
+  public Greeter() {
 
-	}
+  }
 
-	/**
-	 * 
-	 * @param someone
-	 *            person name
-	 * @return string containing Hello + someone
-	 */
-	public final String greet(final String someone) {
-		return String.format("Hello, %s!", someone);
-	}
+  /**
+   * 
+   * @param someone
+   *          person name
+   * @return string containing Hello + someone
+   */
+  public final String greet(final String someone) {
+    return String.format("Hello, %s!", someone);
+  }
 }
