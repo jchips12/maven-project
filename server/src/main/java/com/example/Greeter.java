0420d0e1dbaff3ef03efdf0d991092410f2a736c
JC Hipolito <jchips12@gmail.com>
2018-05-28 17:14:33 +0800

    checkstyle cleanup

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -17,7 +17,7 @@ public class Greeter {
    * @param someone
    * @return a string containing Hello + someone
    */
-  public final String greet(String someone) {
+  public String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
